a830cd4c2b06771d08fc0792d061dffe75e1374d
Alzaros <79316474+Alzaros@users.noreply.github.com>
2023-03-12 14:20:24 +0100

    Last

--- Toolbox/installation.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Installation des différents pakages
-holehe_command = f"pip install holehe nmap python-nmap outcome requests tk scapy prettytable"
-os.system(holehe_command)
---
+++ installation.py
@@ -0,0 +1,5 @@
+import os
+
+# Installation des différents pakages
+holehe_command = f"pip install holehe python-nmap outcome requests tk scapy prettytable"
+os.system(holehe_command)
